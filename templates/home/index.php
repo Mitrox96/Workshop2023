3922f9742348e557e9f1b46afa55e6bc8c0c00b7
JARDY Anthony <103176740+despiow@users.noreply.github.com>
2023-10-03 10:55:07 +0200

    modification d'index.php

--- templates/home/index.php
+++ templates/home/index.php
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../ressources/css/style.css">
-    <title>Page d'accueil</title>
-</head>
-<body>
-    <?php include('header.php'); ?>
-
-    <?php include('nav.php'); ?>
-
-    <main>
-    
+<?php
+session start;
+require('header.php');
+?>
         <div class="container">
 
         <h1 class="Titre" style="text-align: center;">Bienvenu sur EduPartage</h1>
@@ -29,10 +17,6 @@
 </body>
 </html>
 
-!
-=======
 <?php
-session start;
-require('header.php');
+require('footer.php');
 ?>
->>>>>>> d89c60e5c0340685ea11c8e8a28c011a0329bae3
