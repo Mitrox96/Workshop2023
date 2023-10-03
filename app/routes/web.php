6d1cb554f3040c336f97ecf4502c714254b7f12d
Damien LOUBIOU <damien.loubiou@epsi.fr>
2023-10-03 11:22:24 +0200

    reset project

--- app/routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use App\Route;
-
-Route::get(['/', 'App\Controllers\HomeController@index'])->name('Accueil');
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-define('__BASEPATH__',__DIR__);
-require 'vendor/autoload.php';
-require __BASEPATH__. '/app/functions.php';
-
-$app = App\Kernel::init();
