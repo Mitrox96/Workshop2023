f60f20617a6fc303a0b65e63dc4d5cdd3c4c18c8
JARDY Anthony <103176740+despiow@users.noreply.github.com>
2023-10-03 10:46:13 +0200

    changement du header

--- templates/home/commons/header.php
+++ templates/home/commons/header.php
@@ -1,11 +1,19 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Framework</title>
-        <link rel="stylesheet" href="public/css/app.css">
-</head>
-<body>
+    <title><?= $title ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="ressources/css/style.css">
    
+</head>
+<body >
---
+++ templates/home/index.php
@@ -0,0 +1,4 @@
+<?php
+session start;
+require('header.php');
+?>
