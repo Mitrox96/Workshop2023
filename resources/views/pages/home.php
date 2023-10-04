59569172ac71ac56e5c2ad7560816157affe3d09
Jean ROBERT <jean_robert92@live.fr>
2023-10-04 10:41:13 +0200

    Ajout de la base du footer

--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -4,11 +4,7 @@
 
         <img src="public/images/20859175_6347087.jpg" alt="" class="img1">
 
-<<<<<<< HEAD
         <a href="<?= route('Login') ?>" class="lienConnex"><label for="">Page de connexion</label></a>
-=======
-        <a href="<?= route('Login')?>" class="lienConnex"><label for="">Page de connexion</label></a>
->>>>>>> 013afdf7ee67de25faa43a5b40be260648888bb6
 
     </div>
 </main> 
