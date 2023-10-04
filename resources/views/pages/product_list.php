2eadcfef71c14260cc196b66b681d6556b6c6d54
Axel.D <daguerre.axel@gmail.com>
2023-10-04 14:04:50 +0200

    Ajout des cursus dans le nav

--- resources/views/pages/product_list.php
+++ resources/views/pages/product_list.php
@@ -11,9 +11,40 @@
 <header class="header-container">
     <button id="accueil-btn" class="button-primary">Accueil</button>
     <select id="cursus-dropdown" class="button-primary">
-        <option value="informatique">Sélectionner </option>
-        <option value="mathematiques">Mathématiques</option>
-        <option value="physique">Physique</option>
+        <option value="">Quel-est ton cursus ? </option>
+                        <option value="goldfish">Licence en Informatique - Première année</option>
+                        <option value="goldfish">Licence en Informatique - Deuxième année </option>
+                        <option value="goldfish">Licence en Informatique - Troisième année </option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Première année</option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Deuxième année</option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Troisième année</option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Quatrième année </option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Cinquième année</option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Sixième année</option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Septième année</option>
+                        <option value="goldfish">Doctorat en Biologie Moléculaire - Huitième année </option>
+                        <option value="goldfish">Licence en Psychologie - Première année</option>
+                        <option value="goldfish">Licence en Psychologie - Deuxième année</option>
+                        <option value="goldfish">Licence en Psychologie - Troisième année</option>
+                        <option value="goldfish">Master en Architecture - Première année</option>
+                        <option value="goldfish">Master en Architecture - Deuxième année</option>
+                        <option value="goldfish">Master en Architecture - Troisième année</option>
+                        <option value="goldfish">Master en Architecture - Quatrième année </option>
+                        <option value="goldfish">Master en Architecture - Cinquième année </option>
+                        <option value="goldfish">Licence en Histoire de l'Art - Première année </option>
+                        <option value="goldfish">Licence en Histoire de l'Art - Deuxième année</option>
+                        <option value="goldfish">Licence en Histoire de l'Art - Troisième année</option>
+                        <option value="goldfish">Doctorat en Médecine - Première année</option>
+                        <option value="goldfish">Doctorat en Médecine - Deuxième année</option>
+                        <option value="goldfish">Doctorat en Médecine - Troisième année</option>
+                        <option value="goldfish">Doctorat en Médecine - Quatrième année</option>
+                        <option value="goldfish">Doctorat en Médecine - Cinquième année</option>
+                        <option value="goldfish">Doctorat en Médecine - Sixième année</option>
+                        <option value="goldfish">Doctorat en Médecine - Septième année </option>
+                        <option value="goldfish">Doctorat en Médecine - Huitième année </option>
+                        <option value="goldfish">Doctorat en Médecine - Neuvième année </option>
+                        <option value="goldfish">Doctorat en Médecine - Dixième année </option>
+                        <option value="goldfish">Doctorat en Médecine - Onzième année </option>
         <!-- Ajoutez d'autres options au besoin -->
     </select>
     <a href="#" id="profil-btn" class="button-image">
